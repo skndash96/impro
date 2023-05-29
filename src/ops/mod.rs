39c1da5fd4f4918aa19d8c7a28801a00c6cfc98e
Dash Skndash <dashskndash@gmail.com>
2023-05-29 07:55:12 +0530

    new (#2)

--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -9,6 +9,13 @@ use image::ImageBuffer;
 pub type OpImage = ImageBuffer<Rgba<u8>, Vec<u8>>;
 pub type OpResult<T> = Result<T, Box<dyn std::error::Error>>;
 
+pub enum Kernel {
+    K3 ([i32; 9], i32),
+    K5 ([i32; 25], i32),
+    Kc3 ([i32; 9*CH_LEN], i32),
+    Kc5 ([i32; 25*CH_LEN], i32)
+}
+
 pub const CH_LEN : usize = 4;
 
 pub mod crop;
